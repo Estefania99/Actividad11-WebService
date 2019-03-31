a68071a89ad575dbd78d3e15c1a277486deddb3a
Estefania99 <1717110652@utectulancingo.edu.mx>
2019-03-30 18:53:33 -0600

    Actualizacion de base de datos

--- data/schema.sql
+++ data/schema.sql
@@ -1,34 +1,5 @@
 CREATE DATABASE ferreteria_egr;
-
-USE ferreteria_egr;
-
-CREATE TABLE users(
-    username varchar(20) NOT NULL PRIMARY KEY,
-    password varchar(32) NOT NULL,
-    privilege integer NOT NULL DEFAULT -1,
-    status integer NOT NULL DEFAULT 1,
-    name varchar(150) NOT NULL,
-    email varchar(100) NOT NULL,
-    other_data varchar(50) NOT NULL,
-    user_hash varchar(32) NOT NULL,
-    change_pwd integer NOT NULL DEFAULT 1,
-    created timestamp NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE sessions(
-    session_id char(128) UNIQUE NOT NULL,
-    atime timestamp NOT NULL default current_timestamp,
-    data text
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE logs( 
-    id_log integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username varchar(20) NOT NULL,
-    ip varchar(16) NOT NULL,
-    access timestamp NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+use ferreteria_egr;
 
 CREATE TABLE clientes( 
     id_cliente integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -41,19 +12,12 @@ CREATE TABLE clientes(
 
 INSERT INTO clientes(nombre,apellido_paterno,apellido_materno,telefono,email)VALUES
 ('Alexis','Hernandez','Perez', '7714563289','alexisher@hotmail.com'),
-('Angeles','Gayosso','Octaviano', '5512369850','angelesocta14@gmail.com'),
+('Estefania','Garcia','Resendiz', '5512369850','fannygarcia14@gmail.com'),
 ('Carlos','Dominguez','Omania', '7751235698','carlitosdom12@gmail.com');
 
-
-
-INSERT INTO users (username, password, privilege, status, name, email, other_data, user_hash, change_pwd)
-VALUES ('admin',MD5(concat('admin', 'kuorra_key')), 0, 1, 'Admin', 'admin@gmail.com','TIC:SI', MD5(concat('admin', 'kuorra_key', '2016/06/04')), 0),
-('guess',MD5(concat('guess', 'kuorra_key')), 1, 1, 'Guess', 'guess@gmail.com','TIC:SI', MD5(concat('guess', 'kuorra_key','2016/06/04')), 0);
-
-
-SELECT * FROM users;
-SELECT * FROM sessions;
 
 CREATE USER 'fan1'@'localhost' IDENTIFIED BY 'fan1.2019';
 GRANT ALL PRIVILEGES ON ferreteria_egr.* TO 'fan1'@'localhost';
 FLUSH PRIVILEGES;
+
+
