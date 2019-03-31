4e3fd431b918c7d4a59abc80eb55f50aae5698ee
Estefania99 <1717110652@utectulancingo.edu.mx>
2019-03-30 19:51:39 -0600

    Actualizacion

--- application/controllers/logs/url.md
+++
@@ -1,3 +0,0 @@
-'/logs', 'application.controllers.logs.index.Index',
-'/logs/printer', 'application.controllers.logs.printer.Printer',
-'/logs/view/(.+)', 'application.controllers.logs.view.View',
--- application/controllers/main/config.py
+++
@@ -1,9 +0,0 @@
-import web
-import application.models.model_users
-import application.models.model_logs
-
-render = web.template.render('application/views/main/', base='master')
-model = application.models.model_users
-model_logs = application.models.model_logs
-
-secret_key = "kuorra_key"
