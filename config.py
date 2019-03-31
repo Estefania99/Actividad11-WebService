6b3ecd0821a2b582f6099a199deb9d4316b29f8e
Estefania99 <1717110652@utectulancingo.edu.mx>
2019-03-30 18:33:47 -0600

    se actualiza la conexion de forma remota

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import web
 
 db = web.database(
     dbn='mysql',
-    host='localhost',
-    db=  'ferreteria_egr',
-    user= 'fan1',
-    pw='fan1.2019',
+    host='nt71li6axbkq1q6a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    db=  'lk1afc1dz85dmcj0',
+    user= 'kxro5t0ew6v97vt2',
+    pw='auewas4jegpv4kb5',
     port=3306
     )
     
